2ca222419884df6a07d8b0e2d14864cb9046f201
Ambogo2 <ruthambogo.ra@gmail.com>
2024-04-14 20:25:20 +0300

    create user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS  hbnb_dev_db;
 
--- creates user and grants priviledges
+-- creates user and grants privileges
 CREATE USER IF NOT EXISTS  'hbnb_dev' @'localhost' IDENTIFIED  BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEDGES  ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+GRANT ALL PRIVILEGES  ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEDGES;
+FLUSH PRIVILEGES;
